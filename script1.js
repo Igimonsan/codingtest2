4f03344a8a85456fed37ad020af783848f33eb84
Igimonsan <akuntb101010@gmail.com>
2025-03-27 21:01:48 +0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+function animateProject(element) {
+    element.style.transform = "scale(1.1)";
+    element.style.transition = "transform 0.3s ease";
+}
+
+function resetProject(element) {
+    element.style.transform = "scale(1)";
+}
