1f1da5bfee32653679208fe3cd6f5d91df07afcf
Igimonsan <akuntb101010@gmail.com>
2025-03-27 21:15:11 +0700

    Delete script1.js

--- script1.js
+++
@@ -1,8 +0,0 @@
-function animateProject(element) {
-    element.style.transform = "scale(1.1)";
-    element.style.transition = "transform 0.3s ease";
-}
-
-function resetProject(element) {
-    element.style.transform = "scale(1)";
-}
